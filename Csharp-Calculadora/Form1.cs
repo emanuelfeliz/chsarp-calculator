8eaf64afcec99f7028e7d72b96b9a83383709d7f
emanuelfeliz <emanuel.felizmendez@gmail.com>
2022-02-08 21:50:53 -0400

    remaining changes

--- Csharp-Calculadora/Form1.cs
+++ Csharp-Calculadora/Form1.cs
@@ -10,11 +10,131 @@ using System.Windows.Forms;
 
 namespace Csharp_Calculadora
 {
-    public partial class Form1 : Form
+    public partial class Calc : Form
     {
-        public Form1()
+        private double valor1;
+        private double valor2;
+
+        private double resultado;
+
+        private int operacion;
+
+        public Calc()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            operacion = 2;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            resultado = valor1 -   valor2;
+            tbDisplay.Text = "";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "9";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "6";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "3";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = tbDisplay.Text + "0";
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            operacion = 1;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            valor2 = Convert.ToDouble(tbDisplay.Text);
+            switch (operacion)
+            {
+                case 1:
+                    resultado = valor1 + valor2;
+                    break;
+                case 2:
+                    resultado = valor1 - valor2;
+                    break;
+                case 3:
+                    resultado = valor1 * valor2;
+                    break;
+                case 4:
+                    resultado = valor1 / valor2;
+                    break;
+
+            }
+           
+            tbDisplay.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operacion = 3;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            operacion = 4;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
         }
     }
 }
